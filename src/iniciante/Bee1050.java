50c382c8e00ee96b83391b31d2adc5d064527705
João Miguel da Silva Sousa <joaodev30@gmail.com>
2025-09-03 04:29:16 -0300

    git commit -m "First commit"

---
+++ src/iniciante/Bee1050.java
@@ -0,0 +1,36 @@
+package iniciante;
+
+import java.util.Scanner;
+
+/**
+ * Classe responsável por resolver o Desafio 1050 do Beecrowd.
+ * Imprime o nome da cidade correspondente ao DDD existente na entrada.
+ * Url do desafio para melhor entendimento: https://judge.beecrowd.com/pt/problems/view/1050
+ */
+public class Bee1050 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int ddd = scan.nextInt();
+        scan.close();
+
+        if (ddd == 61){
+            System.out.println("Brasilia");
+        } else if (ddd == 71) {
+            System.out.println("Salvador");
+        } else if (ddd == 11) {
+            System.out.println("Sao Paulo");
+        } else if (ddd == 21) {
+            System.out.println("Rio de Janeiro");
+        } else if (ddd == 32) {
+            System.out.println("Juiz de Fora");
+        } else if (ddd == 19) {
+            System.out.println("Campinas");
+        } else if (ddd == 27) {
+            System.out.println("Vitoria");
+        } else if (ddd == 31) {
+            System.out.println("Belo Horizonte");
+        }else {
+            System.out.println("DDD nao cadastrado");
+        }
+    }
+}
